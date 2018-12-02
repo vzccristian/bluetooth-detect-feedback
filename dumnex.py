3282f0e3fbf819b309b8d2f88ed221f401f22016
Cristian Vázquez <vzccristian@gmail.com>
2018-12-02 16:13:58 +0100

    Added GUI.

--- dumnex.py
+++ dumnex.py
@@ -4,42 +4,54 @@ import time
 import os
 import yaml
 import random
-import pprint
 from bluetooth import *
 import requests
+import sys
 
-userMac="38:37:8B:C6:80:CB"
-userId="U1"
+userMac = "48:2C:A0:21:5D:39"
+userId = "U1"
 URL_BASE3 = 'https://dumnex-tratamiento.herokuapp.com/v1/valoracion?usuarioId='
 
-with open("feedback.yaml", 'r', encoding='utf8') as stream:
-    try:
-        feedback = yaml.load(stream)
-    except yaml.YAMLError as exc:
-        print(exc)
+if __name__ == '__main__':
 
-while(1):
-    print("Buscando dispositivos bluetooth...")
-    nearby_devices = discover_devices(lookup_names = True)
-    print("Encontrados {} dispositivos: ".format(len(nearby_devices)))
-    print(nearby_devices)
-    for dev in nearby_devices:
-        if (userMac == dev[0]):
-            print("El usuario ha llegado.")
-            name = list(feedback.keys())[0]
-            req = requests.get(URL_BASE3+userId)
-            if (req.status_code == 200):
+    with open("feedback.yaml", 'r', encoding='utf8') as stream:
+        try:
+            feedback = yaml.load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+    name = list(feedback.keys())[0]
+
+    print("Buscando {} bluetooth".format(userMac))
+    while (1):
+        check = 0
+        print("Buscando dispositivos bluetooth...")
+        nearby_devices = discover_devices(lookup_names=True)
+        print("Encontrados {} dispositivos: ".format(len(nearby_devices)))
+        print(nearby_devices)
+
+        for dev in nearby_devices:
+            check += 1
+            if userMac == dev[0]:
+                print("El usuario ha llegado.")
                 try:
-                    valoracion = int(req.text)
-                    texto = feedback[name][valoracion][random.randint(0, len(feedback[name][valoracion]))-1]
-                    print("Valoración de hoy: {}. \n{}".format(valoracion,texto))
-                    mytext = "Hola {}. {}".format(name, texto)
-                    myobj = gTTS(text=mytext, lang="es", slow=False)
-                    myobj.save("tempfile.mp3")
-                    os.popen('mpg321 tempfile.mp3').read()
-                    # os.system("mpg321 tempfile.mp3")
-                    time.sleep(1)
-                    os.system("rm -f tempfile.mp3")
+                    req = requests.get(URL_BASE3 + userId)
+                    if req.status_code == 200:
+                        try:
+                            valoracion = int(req.text)
+                            texto = feedback[name][valoracion][random.randint(0, len(feedback[name][valoracion])) - 1]
+
+                            print("Valoración de hoy: {}. \n{}".format(valoracion, texto))
+
+                            mytext = "Hola {}. {}".format(name, texto)
+
+                            myobj = gTTS(text=mytext, lang="es", slow=False)
+                            myobj.save("tempfile.mp3")
+
+                            os.popen('mpg321 tempfile.mp3').read()
+                            time.sleep(1)
+                            os.system("rm -f tempfile.mp3")
+                        except Exception:
+                            pass
                 except Exception:
                     pass
-    time.sleep(2)
+        time.sleep(2)
---
+++ dumnexUi.py
@@ -0,0 +1,121 @@
+# encoding=utf8
+from gtts import gTTS
+import time
+import os
+import yaml
+import random
+from bluetooth import *
+import requests
+import sys
+from PyQt5 import uic, QtGui, QtCore
+from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QApplication
+import threading
+
+userMac = "48:2C:A0:21:5D:39"
+userId = "U1"
+URL_BASE3 = 'https://dumnex-tratamiento.herokuapp.com/v1/valoracion?usuarioId='
+
+
+class Communicate(QtCore.QObject):
+    myGUI_signal = QtCore.pyqtSignal(int, str)
+
+
+class Display(QMainWindow):
+    def __init__(self):
+        QDialog.__init__(self)
+        uic.loadUi("display.ui", self)
+        self.setWindowTitle("DUMNEX")
+        self.texto.setWordWrap(True)
+        try:
+            self.t = threading.Thread(name='qtThread', target=qtThread, args=(self.setFeedback,))
+            self.t.start()
+        except (KeyboardInterrupt, SystemExit):
+            self.t._stop()
+
+    def setFeedback(self, valoracion, texto):
+        print("V:{} T:{}".format(valoracion, texto))
+        try:
+            if valoracion > 0:
+                self.valoracion.setText(str(valoracion))
+            else:
+                self.valoracion.setText("")
+        except Exception:
+            print("Exception.")
+            self.valoracion.setText("")
+        try:
+            self.texto.setText(texto)
+        except Exception:
+            print("Exception.")
+            self.texto.setText("")
+
+    def kill(self):
+        self.t.do_run = False
+
+
+def qtThread(callback):
+    mysrc = Communicate()
+    mysrc.myGUI_signal.connect(callback)
+    t = threading.currentThread()
+    encontrado = False
+    with open("feedback.yaml", 'r', encoding='utf8') as stream:
+        try:
+            feedback = yaml.load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+    name = list(feedback.keys())[0]
+    mysrc.myGUI_signal.emit(-1, "Esperando por "+name)  # Emit
+    print("Buscando dispositivo: {}".format(userMac))
+
+    while getattr(t, "do_run", True):
+        # Main Code
+
+        check = 0
+        print("Buscando dispositivos bluetooth...")
+        nearby_devices = discover_devices(lookup_names=True)
+        print("Encontrados {} dispositivos: ".format(len(nearby_devices)))
+        print(nearby_devices)
+
+        for dev in nearby_devices:
+            check += 1
+            if userMac == dev[0]:
+                print("El usuario ha llegado.")
+                if not encontrado:
+                    encontrado = True
+                    try:
+                        req = requests.get(URL_BASE3 + userId)
+                        if req.status_code == 200:
+                            try:
+                                valoracion = int(req.text)
+                                texto = feedback[name][valoracion][random.randint(0, len(feedback[name][valoracion])) - 1]
+                                print("Valoración de hoy: {}. \n{}".format(valoracion, texto))
+                                mytext = "Hola {}. {}".format(name, texto)
+
+                                mysrc.myGUI_signal.emit(valoracion, mytext)  # Emit
+
+                                myobj = gTTS(text=mytext, lang="es", slow=False)
+                                myobj.save("tempfile.mp3")
+
+                                os.popen('mpg321 tempfile.mp3').read()
+                                time.sleep(1)
+                                os.system("rm -f tempfile.mp3")
+                            except Exception:
+                                pass
+                    except Exception:
+                        pass
+            elif (len(nearby_devices) == check):
+                encontrado = False
+                mysrc.myGUI_signal.emit(-1, "Esperando por "+name)  # Emit
+        if len(nearby_devices) == 0:
+            encontrado = False
+
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    mainWindow = Display()
+    mainWindow.show()
+    app.exec()
+    mainWindow.kill()
